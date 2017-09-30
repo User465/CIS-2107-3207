6614900bf424147282ea8b879a66a63b75c2d4a8
thatyanamorales <thtnmorales77@gmail.com>
2017-09-30 17:58:20 -0400

    All of the built in functions work (except help)

---
+++ CIS 3207 Operating_Systems/builtin.h
@@ -0,0 +1,168 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <dirent.h>
+
+
+int shell_cd(char **args);
+int shell_clr(char **args);
+int shell_dir(char **args);
+int shell_environ(char **args);
+int shell_echo(char **args);
+int shell_help(char **args);
+int shell_pause(char **args);
+int shell_quit(char **args);
+
+int isBuiltIn(char* argv[]);
+#define commandSize 7
+extern char **environ;
+
+
+char* builtin_command[] =
+{
+  "cd",
+  "clr",
+  "dir",
+  "environ",
+  "echo",
+  "help",
+  "pause",
+};
+
+int (*builtin_function[])(char **) =
+{
+  &shell_cd, //check
+  &shell_clr, //check
+  &shell_dir, //check
+  &shell_environ, //check
+  &shell_echo, //check
+  &shell_help,
+  &shell_pause, //check
+};
+
+int isBuiltIn(char* argv[])
+{
+  int i;
+  for(i = 0; i < sizeof(*builtin_command); i++)
+  {
+    if(strcmp(argv[0], builtin_command[i]) == 0)
+    {
+      return 1;
+    }
+  }
+  return 0;
+}
+
+
+int shell_cd(char **args)
+{
+  if(args[1] == NULL)
+  {
+    char cwd[1024];
+    getcwd(cwd, sizeof(cwd));
+    printf("Current directory is: %s\n", cwd);
+  }
+  else
+  {
+    if(chdir(args[1]) != 0)
+    {
+      perror("cd error\n");
+    }
+  }
+}
+
+int shell_clr(char **args)
+{
+  system("clear");
+}
+
+int shell_dir(char **args)
+{
+  DIR *directory;
+  struct dirent *reader;
+
+  directory = opendir ("./");
+  if (directory != NULL)
+   {
+     while (reader = readdir (directory))
+       puts (reader->d_name);
+     (void) closedir (directory);
+   }
+  else
+   perror ("Error: Directory couldn't be opened");
+
+  return 0;
+}
+
+int shell_environ(char **args)
+{
+  int i = 1;
+  char *s = *environ;
+
+  for (; s; i++)
+  {
+    printf("%s\n", s);
+    s = *(environ+i);
+  }
+
+  return 0;
+}
+
+
+int shell_echo(char **args)
+{
+  int i = 1;
+  while(args[i] != NULL)
+  {
+    printf("%s ", args[i]);
+    i++;
+  }
+  printf("%s", "\n");
+}
+
+int shell_help(char **args)
+{
+  // FILE *fp;
+  // int a = shell_clr(args);
+  // int c;
+  // int n = 0;
+  //
+  // fp = fopen("UserManual.txt","r");
+  // if(fp == NULL)
+  // {
+  //   perror("Error");
+  //   return(0);
+  // }
+  // do
+  // {
+  //   c = fgetc(fp);
+  //   if(feof(fp))
+  //   {
+  //      break ;
+  //   }
+  //   printf("%c", c);
+  // }while(1);
+  //
+  // fclose(fp);
+  // return(0);
+
+  // int i;
+  // printf("These are built in\n");
+  //
+  // for (i = 0; i < commandSize; i++) {
+  //   printf("  %s\n", builtin_command[i]);
+  }
+
+
+int shell_pause(char **args)
+{
+  char buffArray[500] = " ";
+  printf("Shell is paused. Press enter to continue\n");
+
+  while(strcmp((buffArray), "\n") != 0)
+  {
+    fgets(buffArray, sizeof(buffArray), stdin);
+  }
+  printf("Shell has resumed\n");
+}
--- CIS 3207 Operating_Systems/shell.c
+++ CIS 3207 Operating_Systems/shell.c
@@ -11,7 +11,7 @@ Input/Output of Each Function
 **Include discussion of each possible function return value
 
 */
-
+#include <sys/wait.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -19,26 +19,32 @@ Input/Output of Each Function
 #include <sys/time.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include "builtin.h"
 
 #define arrSize 1024
+#define commandSize 7
 
 char* readALine();
 void printHostName();
-void evaluate(char *line);
+int evaluate(char *line);
 void parseLine(char *line, char* argv[arrSize]);
+void forkProgram();
+int runBuiltIn(char* argv[arrSize]);
 
 int main(int argc, char* argv[])
 {
   char *lineInput;
+  system("clear");
   while(1) {
     printHostName();
     lineInput = readALine();
-    if(strcmp(lineInput, "exit\n") == 0 || feof(stdin))
+    if(strcmp(lineInput, "quit\n") == 0 || feof(stdin))
     {
-      puts(" ");
       return EXIT_SUCCESS;
     }
-    evaluate(lineInput);
+    int a = evaluate(lineInput);
   } //end while
 }
 
@@ -65,14 +71,28 @@ void printHostName()
        perror("getcwd() error");
 }
 
-void evaluate(char *line)
+int evaluate(char *line)
 {
   char* argv[arrSize]; //array of pointers to chars
 
   parseLine(line, argv);
 
-  //rest of code!
+  if(argv[0] == NULL)
+  {
+    return 0; //ignore empty lines
+  }
 
+  if (isBuiltIn(argv) == 1)
+  {
+    runBuiltIn(argv);
+  }
+  else
+  {
+    printf("%s", "\n");
+    //forkProgram();
+    return 1;
+  }
+  return 1;
 }
 
 void parseLine(char *line, char* argv[arrSize])
@@ -91,4 +111,17 @@ void parseLine(char *line, char* argv[arrSize])
     argv[counter] = token;
     counter++;
   }
+}
+
+int runBuiltIn(char* argv[arrSize])
+{
+  int i;
+  for(i = 0; i < commandSize; i++)
+  {
+    if(strcmp(argv[0], builtin_command[i]) == 0)
+    {
+      return (*builtin_function[i])(argv);
+    }
+  }
+  return 0;
 }
